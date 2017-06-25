d72edf250ce76c38d0938a9c68316deadde0b0c8
James <jayms.portune@gmail.com>
2017-06-24 21:02:35 -0700

    Saving Work Push

--- javascript/prices.js
+++ javascript/prices.js
@@ -1,30 +1,29 @@
 'use strict'
 debugger;
-var table = document.getElementById('generatedTable');
-var createTHead = document.createElement('th');
-var createTRow = document.createElement('tr');
-var createTData = document.createElement('td');
+var fapStore = new StoreData('First and Pike', 23, 65, 6.3);
+var saStore = new StoreData('Seatac Airport', 3, 24, 1.2);
+var scStore = new StoreData('Seattle Center', 11, 38, 3.7);
+var chStore = new StoreData('Capital Hill', 20, 38, 2.3);
+var aStore = new StoreData('Alki', 2, 16, 4.6);
+var stores = [fapStore, saStore, scStore, chStore, aStore];
+var hour = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm']
 
-var storeData = {
-  name: ['1st and Pike', 'Seatac Airport', 'Seattle Center', 'Capital Hill', 'Alki'],
-  hour: ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'],
-  cxVisit: cxVisit;
+function StoreData(name, minCx, maxCx, avgCookies) {
+  this.name = name;
+  this.avgCookies = avgCookies;
+  this.minCx = minCx;
+  this.maxCx = maxCx;
+  this.cookiesPerHour = [];
 }
-for (var i = 0; i<= storeData.name.length; i++){
-  console.log("I is = " + i);
-  for (var j = 0; j<= storeData.hour.length; j++){
-    console.log("J is = " + j);
-    var name = storeData.name[i];
-    var cxVisit = ranCxCount();
-    var hour = storeData.hour[j];
-    var location = new Object(name, hour, cxVisit)
-    location.name = name;
-    location.cxVisit = cxVisit;
-    location.hour = hour;
-  }
+
+function test() {
+  var ranNum = Math.floor(Math.random() * (stores[i].maxCx - stores[i].minCx)) + stores[i].minCx;
+  this.cookiesPerHour.push(pta);
 }
-//document.getElementById('generatedTable').appendChild.write(createTHead);
-function ranCxCount () {
-  var ranCxCountPerHour = Math.floor(Math.random() * 20);
- return ranCxCountPerHour;
+function randomNum(minCx, maxCx){
+  var ranNum = Math.floor(Math.random() * (maxCx - minCx)) + minCx;
+  return ranNum;
+}
+for (var i = 0; i < stores.length; i++){
+  test(stores[i]);
 }
