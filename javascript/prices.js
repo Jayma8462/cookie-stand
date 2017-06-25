7895e2f584891bce43c6660dc42c2331fee84e93
Jayms8462 <Jayms.Portune@gmail.com>
2017-06-25 16:09:43 -0700

    Finish Product for lab 7?

--- javascript/prices.js
+++ javascript/prices.js
@@ -1,29 +1,53 @@
 'use strict'
-debugger;
 var fapStore = new StoreData('First and Pike', 23, 65, 6.3);
 var saStore = new StoreData('Seatac Airport', 3, 24, 1.2);
 var scStore = new StoreData('Seattle Center', 11, 38, 3.7);
 var chStore = new StoreData('Capital Hill', 20, 38, 2.3);
 var aStore = new StoreData('Alki', 2, 16, 4.6);
 var stores = [fapStore, saStore, scStore, chStore, aStore];
-var hour = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm']
-
+var hour = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+var totalCookiesPerHour = [];
+var allStoresCookieCount = 0;
+var passVar = 0;
 function StoreData(name, minCx, maxCx, avgCookies) {
   this.name = name;
   this.avgCookies = avgCookies;
   this.minCx = minCx;
   this.maxCx = maxCx;
   this.cookiesPerHour = [];
+  var hour = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+  for (var i = 0; i < hour.length; i++){
+    var ranNum = Math.floor(Math.random() * (this.maxCx - this.minCx)) + this.minCx;
+    var cphPush = Math.round(ranNum * this.avgCookies);
+    this.cookiesPerHour.push(cphPush);
+  }
 }
-
-function test() {
-  var ranNum = Math.floor(Math.random() * (stores[i].maxCx - stores[i].minCx)) + stores[i].minCx;
-  this.cookiesPerHour.push(pta);
+function totalCookies(hour, stores) {
+  for (var i = 0; i < hour.length; i++){
+    for (var j = 0; j < stores.length; j++){
+      var total = stores[j].cookiesPerHour[i];
+      allStoresCookieCount += total;
+      if (j === stores.length - 1){
+        totalCookiesPerHour[i] = allStoresCookieCount;
+        allStoresCookieCount = 0;
+      }
+    }
+  }
 }
-function randomNum(minCx, maxCx){
-  var ranNum = Math.floor(Math.random() * (maxCx - minCx)) + minCx;
-  return ranNum;
-}
-for (var i = 0; i < stores.length; i++){
-  test(stores[i]);
+totalCookies(hour, stores);
+window.onload = function(stores) {
+  var stores = [fapStore, saStore, scStore, chStore, aStore];
+  var listEl = document.getElementById('generated-table');
+  var createNewList = document.createElement('ul');
+  var createListEl = document.createElement('li');
+  for (var i = 0; i < stores.length; i++){
+    var storeEl = document.createElement('p');
+    storeEl.textContent = stores[i].name;
+    listEl.appendChild(storeEl);
+    for (var j = 0; j < hour.length; j++){
+      var createEl = document.createElement('li');
+      createEl.textContent = hour[j] + ": " + stores[i].cookiesPerHour[j];
+      listEl.appendChild(createEl);
+    }
+  }
 }
